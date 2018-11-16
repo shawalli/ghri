ca3cab339dc0e408740ce53fad2a069fc5edd453
shawalli <shawn.p.wallis@gmail.com>
2018-11-16 14:29:48 -0500

    moves project information to about file

---
+++ ghri/__about__.py
@@ -0,0 +1,11 @@
+__title__ = "ghri"
+__summary__ = "Display information about GitHub releases."
+__uri__ = "https://github.com/shawalli/ghri"
+
+__version__ = "0.1.0"
+
+__author__ = 'Shawn Wallis'
+__email__ = 'shawn.p.wallis@gmail.com'
+
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2018 {author}'.format(author=__author__)
--- setup.py
+++ setup.py
@@ -1,9 +1,23 @@
+from pathlib import Path
 from setuptools import setup, find_packages
 
+about_file = Path(Path(__file__).parent, "ghri", "__about__.py")
+
+about = {}
+with open(about_file) as f:
+    exec(f.read(), about)
+
 setup(
-    name="ghri",
-    version="0.0.1",
+    name=about["__title__"],
+    version=about["__version__"],
+    description=about["__summary__"],
+    url=about["__uri__"],
+    author=about["__author__"],
+    author_email=about["__email__"],
+    license=about["__license__"],
     packages=find_packages(),
+    zip_safe=False,
+    platforms="any",
     include_package_data=True,
     install_requires=[
         "attrdict",
@@ -18,4 +32,18 @@ setup(
         [console_scripts]
         ghri=ghri.scripts.ghri:cli
     """,
+    python_requires=">=3.7",
+    keywords="github git vcs command-line cli releases python",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Framework :: Click",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Internet :: WWW/HTTP :: Indexing/Search",
+        "Topic :: Software Development :: Version Control :: Git",
+        "Topic :: Utilities",
+    ]
 )
