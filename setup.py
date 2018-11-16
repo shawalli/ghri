29745e5bd4c00330b97c8125138f6d7901208994
shawalli <shawn.p.wallis@gmail.com>
2018-11-16 15:36:05 -0500

    adds support for python versions 3.5 and 3.6

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
         [console_scripts]
         ghri=ghri.scripts.ghri:cli
     """,
-    python_requires=">=3.7",
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
     keywords="github git vcs command-line cli releases python",
     classifiers=[
         "Development Status :: 3 - Alpha",
@@ -41,6 +41,9 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Topic :: Internet :: WWW/HTTP :: Indexing/Search",
         "Topic :: Software Development :: Version Control :: Git",
